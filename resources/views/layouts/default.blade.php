6e3ed077bc5ec9e5472502896fc24b09eab7bc4a
codeEver <241998184@qq.com>
2019-05-17 15:30:04 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,7 +8,8 @@
     @include('layouts._header')
 
     <div class="container">
-      <div>
+      <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
       </div>
