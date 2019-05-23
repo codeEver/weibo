6b696151ec73faf96e0471f12a77e3dc3820905d
codeEver <241998184@qq.com>
2019-05-23 03:30:36 +0000

    登陆成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Auth;
 
 class UsersController extends Controller
 {
@@ -30,9 +30,10 @@ class UsersController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email'=> $request->email,
-            'password' => bcrypt($request->passwoed),
+            'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success','欢迎，您将在这里开启一段新的旅程～');
         return redirect()->route('users.show',[$user]);
     }
